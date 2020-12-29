3544d4798682c080ca74ffacaf9882ca650766ae
oleg.rovenskyi <oleg.rovenskyi@dev-pro.net>
2020-12-29 22:46:38 +0200

    update typings

--- src/app/features/chat-room/chat-room.models.ts
+++ src/app/features/chat-room/chat-room.models.ts
@@ -12,9 +12,9 @@ import { ValidatorFn } from '@angular/forms';
 
 export type FormGroupConfig<T> = {
   [key in keyof T]:  T[key]
-  | [T[key]]
-  | [
-  T[key],
-    ValidatorFn | ValidatorFn[]
-];
+    | [T[key]]
+    | [
+    T[key],
+      ValidatorFn | ValidatorFn[]
+  ];
 };
